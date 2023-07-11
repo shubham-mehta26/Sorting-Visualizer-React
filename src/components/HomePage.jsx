b0800a02a74347b9e5823b9a8006f5e8f2cd54f4
Shubham Mehta <shubham.sm911@gmail.com>
2023-07-11 17:19:03 +0530

    scramble-text

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function HomePage() {
+const scramble = require('@ignatiusmb/scramble');
+scramble(document.querySelector('.scamble-text')).run();
+
   return (
     <div>
       <div className="scramble-contailer">
